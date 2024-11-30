870b5018916f7482a120266ec76d00b263db11cf
weikikiii <132288830+weikikiii@users.noreply.github.com>
2024-11-30 21:09:37 +0800

    Update fishman_db_design.sql
    更新內容:
    1. 將資料順序調整成和資料表相同
    2. 新增notification table
    3. 依照後端提供資料表的修改table中的項目

--- fishman_db_design.sql
+++ fishman_db_design.sql
@@ -4,19 +4,18 @@ USE `fisherman_records`;
 
 CREATE TABLE `crew_members` (
     `worker_id` INT PRIMARY KEY, -- 員工編號，船長為0
-    `given_name` VARCHAR(50) NOT NULL, -- 名
-    `family_name` VARCHAR(50) NOT NULL, -- 姓
-    `photo` MEDIUMBLOB, -- 照片
+    `name` VARCHAR(100) NOT NULL, -- 名
     `age` INT CHECK (age > 0 AND age <= 120), -- 年齡，正數，合理範圍1~120
     `country` VARCHAR(100) NOT NULL, -- 國籍
     `passport_number` VARCHAR(20) UNIQUE NOT NULL, -- 護照號碼，需唯一，最多20字
-    `job_title` ENUM('engineer', 'fisherman', 'fish processor', 'deckhand', 'cook') NOT NULL -- 工種，限制為列舉選項
+    `job_title` ENUM('engineer', 'fisherman', 'fish processor', 'deckhand', 'chef') NOT NULL,  -- 工種，限制為列舉選項
+    `profilePhoto` MEDIUMBLOB -- 大頭照
 );
 
 CREATE TABLE `user_login` (
     `username` VARCHAR(100) NOT NULL PRIMARY KEY, -- 使用者姓名，PRIMARY KEY
     `worker_id` INT UNIQUE NOT NULL, -- 員工編號，需唯一，船長為0
-    `pattern` VARCHAR(20) NOT NULL, -- 圖形登入順序，最多20字
+    `pattern` VARCHAR(9) NOT NULL, -- 圖形登入順序，最多9字
     `role` ENUM('captain', 'fisherman') NOT NULL, -- 身分選擇，captain 或 fisherman
     `login_timestamp` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 登入時間，自動記錄當前時間
     `last_login` DATETIME, -- 上次登入時間
@@ -31,26 +30,54 @@ CREATE TABLE `user_login` (
 CREATE TABLE `work_hours` (
     `worker_id` INT NOT NULL, -- 員工編號
     `date` DATE NOT NULL, -- 日期
-    `status` ENUM('working', 'resting', 'dinning') NOT NULL, -- 狀態，只能選擇 working, resting, 或 dinning
-    `timeStart` FLOAT NOT NULL, -- 開始時刻，單位: 小時
-    `timeEnd` FLOAT NOT NULL, -- 結束時刻，單位: 小時
-    `duration` FLOAT GENERATED ALWAYS AS (timeEnd - timeStart) STORED, -- 時長，根據 timeStart 和 timeEnd 自動計算
-    `comments` VARCHAR(255), -- 備註，儲存身體狀態或加班等描述
-    CHECK (`timeEnd` > `timeStart`),
-    PRIMARY KEY (`worker_id`, `date`, `timeStart`), -- Compound Key，避免同一天、同時間重複記錄
+    `status` VARCHAR(20), -- 狀態，0=休息, 1=工作, 2=吃飯
+    `duration` INT NOT NULL, -- 時長，幾個半小時
+    `profilePhoto` MEDIUMBLOB NOT NULL, -- 大頭照
+    `signaturePhoto` MEDIUMBLOB NOT NULL, -- 簽名照片
+    `check` TINYINT(1) NOT NULL,
+    `comments` VARCHAR(255) NOT NULL, -- 備註，儲存身體狀態或加班等描述
+    PRIMARY KEY (`worker_id`, `date`), -- Compound Key，避免同一天重複記錄
     FOREIGN KEY (`worker_id`) REFERENCES `crew_members`(`worker_id`) -- 連結到`crew_members`表中的`worker_id`
     ON DELETE CASCADE -- 當對應的漁工刪除時，刪除相關工作時數記錄
+);
+
+-- 輸入時使用0-2表示狀態，但以文字方式儲存
+DELIMITER $$
+CREATE TRIGGER before_insert_work_hours
+BEFORE INSERT ON work_hours
+FOR EACH ROW
+BEGIN
+    IF NEW.status = '0' THEN
+        SET NEW.status = 'resting';
+    ELSEIF NEW.status = '1' THEN
+        SET NEW.status = 'working';
+    ELSEIF NEW.status = '2' THEN
+        SET NEW.status = 'dinning';
+    END IF;
+END$$
+DELIMITER ;
+
+CREATE TABLE `notification` (
+	`notification_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- 狀態編號
+    `content` TEXT, -- 通知的具體內容
+    `type` varchar(30) 	DEFAULT 'Reminder' , -- 通知類型
+    `status` varchar(15) NULL DEFAULT 'Unread', -- read/unread
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- 資料插入時間
+    `updateAt` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP -- 資料更新時間
 );
 
 DESCRIBE `user_login`;
 DESCRIBE `crew_members`;
 DESCRIBE `work_hours`;
+DESCRIBE `notification`;
 
 SELECT * FROM `user_login`;
 SELECT * FROM `crew_members`;
 SELECT * FROM `work_hours`;
+SELECT * FROM `notification`;
 
 DROP TABLE `user_login`;
-DROP TABLE `crew_members`;
 DROP TABLE `work_hours`;
+DROP TABLE `crew_members`;
+DROP TABLE `notification`;
 DROP DATABASE `fisherman_records`;

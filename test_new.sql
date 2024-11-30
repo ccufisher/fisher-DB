c541c8eecee441c99aee9e439559d0183394270e
xinyu20 <134705449+xinyu20@users.noreply.github.com>
2024-12-01 01:07:11 +0800

    Update test_new.sql

--- test_new.sql
+++ test_new.sql
@@ -3,8 +3,8 @@ USE `fisherman_records`;
 
 -- 測試 crew_members 表格
 INSERT INTO `crew_members` (`worker_id`, `name`, `age`, `country`, `passport_number`, `job_title`, `profilePhoto`)
-VALUES (1, 'JohnDoe', 30, 'USA', 'USA1234567890', 'engineer', LOAD_FILE('C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\jane.png')),
-       (2, 'JaneSmith',  25, 'Canada', 'CAN1234567890', 'fisherman', LOAD_FILE('C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\jane.png'));
+VALUES (1, 'JohnDoe', 30, 'USA', 'USA1234567890', 'engineer', LOAD_FILE('C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\janee.jpg')),
+       (2, 'JaneSmith',  25, 'Canada', 'CAN1234567890', 'fisherman', LOAD_FILE('C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\janee.jpg'));
 -- LOAD_FILE中的路徑為圖片儲存位置
 SELECT * FROM `crew_members`;
 
@@ -18,8 +18,21 @@ SELECT * FROM `user_login`;
 
 -- 測試 work_hours 表格
 INSERT INTO `work_hours` (`worker_id`, `date`, `status`, `duration`, `profilePhoto`, `signaturePhoto`, `check`, `comments`)
-VALUES (1, '2024-11-27', '1', 5, LOAD_FILE('C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\jane.png'), LOAD_FILE('C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\jane.png'), 1, 'Normal shift'),
-       (2, '2024-11-27', '0', 12, LOAD_FILE('C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\jane.png'), LOAD_FILE('C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\jane.png'), 0, 'Lunch break');
+VALUES (1, '2024-11-27', '1', 5, LOAD_FILE('C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\janee.jpg'), 
+								 LOAD_FILE('C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\janee.jpg'), 1, 'Normal shift'),
+       (2, '2024-11-27', '0', 12, LOAD_FILE('C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\janee.jpg'), 
+                                  LOAD_FILE('C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\janee.jpg'), 0, 'Lunch break');
+
+-- 測試 notification 表格
+INSERT INTO `notification` (`content`, `type`, `status`, `createdAt`, `updateAt`)
+VALUES ('Worker Tuang Hee has worked overtime for 100 hours on 12/27/2024', 'Reminder', 'Read', now(), now()),
+       ('Worker Jake has worked overtime for 10 hours on 11/15/2024', 'Reminder', 'Unread', now(), now());
+
+
+-- 測試 report 表格
+INSERT INTO `report` (`check`, `worker_id`, `date`, `issue_description`)
+VALUES (1, 1, '2024-11-27', 'wrong working time'),
+       (0, 2, '2024-11-25', 'Error in meal time registration');
 
 -- 檢查 work_hours 表格資料是否插入成功
 SELECT * FROM `work_hours`;
@@ -30,6 +43,8 @@ DELETE FROM `work_hours` WHERE `worker_id` = 2;
 
 SELECT * FROM `user_login`;
 SELECT * FROM `work_hours`;
+SELECT * FROM `notification`;
+SELECT * FROM `report`;
 
 -- 測試資料刪除（刪除整個 crew_members 資料）
 DELETE FROM `crew_members` WHERE `worker_id` = 1;
